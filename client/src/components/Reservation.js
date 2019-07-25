05d64eb6419927a454f969f5718e114b8514168c
PaulArnaud <paul.arnaud@etu.umontpellier.fr>
2019-07-25 12:56:39 +0200

    Reservation front

--- client/src/components/Reservation.js
+++ client/src/components/Reservation.js
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
 import { } from "react-router-dom";
+import axios from "axios";
 
 class Reservation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            services: ""
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:9000/services')
+            .then(response => {
+                this.setState({ services: response.data });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     render() {
+        var listOfService = [];
+        var services = this.state.services;
+        for (var i = 0; i < services.length; i++) {
+            listOfService.push(<option value={services[i]._id} key={services[i]._id}> {services[i].name} </option>);
+        }
         return (
-            <div>
-                <p> Reservation </p>
+            <div className="container">
+                <input type="text" className="timepicker" placeholder="Choississez une heure" />
+                <input type="text" className="datepicker" placeholder="Choississez une date" />
+                <div className="input-field col s12">
+                    <select className="browser-default" defaultValue={'DEFAULT'}>
+                        <option key="0" value="DEFAULT" disabled>Choississez un service</option>
+                        {listOfService}
+                    </select>
+                </div>
             </div>
         );
     }
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import Reservation from "../Reservation";
+
 class Dashboard extends Component {
   onLogoutClick = e => {
     e.preventDefault();
@@ -20,10 +22,14 @@ class Dashboard extends Component {
                 <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
               </p>
             </h4>
-            <button style={{width: "150px",borderRadius: "3px",letterSpacing: "1.5px",marginTop: "1rem"}} onClick={this.onLogoutClick} className="btn btn-large waves-effect waves-light hoverable green accent-3" >
+            <button style={{ width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} onClick={this.onLogoutClick} className="btn btn-large waves-effect waves-light hoverable green accent-3" >
               Logout
             </button>
           </div>
+
+        </div>
+        <div className="row">
+          <Reservation />
         </div>
       </div>
     );
